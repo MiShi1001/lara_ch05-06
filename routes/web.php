5929775e9be1cf4ca58e9a5bcb83bddf105efb9c
SU-RU <oooaa0727@gmail.com>
2017-10-15 07:02:45 +0800

    Alter 正規表達式限制參數

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
-//基礎路由參數+正規表達式限制參數
+//Alter 正規表達式限制參數
+Route::pattern('student_no','s[0-9]{10}');
 Route::get('student/{student_no}',function($student_no){
     return "學號：".$student_no;
-})->where(['student_no'=>'s[0-9]{10}']);
-Route::get('student/{student_no}/score',function($student_no){
-    return "學號：".$student_no."的所有成績";
 });
-//複數個路由參數
-Route::get('student/{student_no}/score/{subject}',function($student_no,$subject){
-    return "學號：".$student_no."的".$subject."成績";
-});
-//選擇性路由參數+正規表達式限制參數
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
     return "學號：".$student_no."的".(is_null($subject))?"所有科目":$subject."成績";
-})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chineseenglish│math)']);
+})->where(['subject'=>'(chinese│english│math)']);
